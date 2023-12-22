dea32fd9f7d62f710c2903044bd81981ae711afe
AmanS3109 <singhaman2122@ternaengg.ac.in>
2023-12-22 18:22:03 +0530

    commit message

--- LEARNINGJS/counter.js
+++ LEARNINGJS/counter.js
@@ -1,13 +1,22 @@
-let counter = 0; //Declaring variable counter
+if(!localStorage.getItem('counter')){
+    localStorage.setItem('counter', 0)
+}
+//let counter = 0; //Declaring variable counter
+
 function count(){ 
+    let counter = localStorage.getItem('counter')
     counter++;
     document.querySelector('h1').innerHTML = counter;
-    if(counter % 10 == 0){
-        alert(`Count is now ${counter}`);
-    }
+    // if(counter % 10 == 0){
+    //     alert(`Count is now ${counter}`);
+    // }
+    localStorage.setItem('counter', counter)
 }
     document.addEventListener('DOMContentLoaded', function(){
+        document.querySelector('h1').innerHTML = localStorage.getItem('counter');
         document.querySelector('button').onclick = count; 
+
+       // setInterval(count, 1000) //Interval
     });
 
             //alert(counter); 
