576ca9a2cf4b9223f1605fa44135718274f49d8a
AmanS3109 <singhaman2122@ternaengg.ac.in>
2024-01-01 15:20:33 +0530

    commit message

---
+++ DSA/Big0.js
@@ -0,0 +1,18 @@
+// Twitter exercise
+
+const arr = [{
+        Tweet:"hi",
+        Year: 2018}, 
+    {
+        Tweet: "Shruti",
+        Year: 2019}, 
+    {
+        Tweet: "my",
+        Year: 2020}, 
+    {
+        Tweet:"Girl",
+        Year: 2021}]
+
+// O(N^2) Space complexity
+
+"Shurti".length; //O(1)
